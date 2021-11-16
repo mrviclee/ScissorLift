7fc02f3e3c305142075041ef59172988396abd1e
saenzjonathan11 <saenzjonathan11@gmail.com>
2021-11-16 05:09:30 -0600

    open and close button

--- website/main.py
+++ website/main.py
@@ -4,12 +4,17 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    if request.method == "POST":
-        print("Post")
-    else:
-        print("Hello")
+    return render_template("home.html", result = {'phy':50,'che':60,'maths':70})
+
+@app.route("/open.html", methods=["GET", "POST"])
+def open():
+    print("run code to open box")
     return render_template("home.html")
 
+@app.route("/close.html", methods=["GET", "POST"])
+def close():
+    print("write code to close box")
+    return render_template("home.html", result = {'phy':50,'che':60,'maths':70})
 
 if __name__ == "__main__":
     app.run(debug=True)
