5d533bfde7aef020d2515bc89045281f29723699
Caleb Herpin <caleb.herpin@gmail.com>
2021-11-16 04:14:11 -0600

    Added templates

--- website/main.py
+++ website/main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        print("Post")
+    else:
+        print("Hello")
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
